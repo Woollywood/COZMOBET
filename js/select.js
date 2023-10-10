128bbcfccc619df679b13d0e0545e2170a6a285d
woollywood <Gerbert.Wells969@yandex.ru>
2023-10-10 11:45:44 +0300

    adaptive

--- js/select.js
+++ js/select.js
@@ -6,50 +6,47 @@ let value_wrap = document.querySelector('.value-wrap');
 let options = document.querySelectorAll('.option');
 let input = document.getElementById('country');
 
-for (let option of options){
-    option.onclick = selectOption;
+for (let option of options) {
+	option.onclick = selectOption;
 }
-
 
 //console.log(deploy_btn);
-deploy_btn.onclick = function(){
+deploy_btn.onclick = function () {
+	if (deploy_btn.getAttribute('data-state') == 'rolled') {
+		options_list.style.display = 'flex';
+		options_list.style.opacity = 1;
+		deploy_btn.setAttribute('data-state', 'deployed');
+		deploy_btn.style.rotate = '180deg';
+	} else {
+		options_list.style.opacity = 0;
+		options_list.style.display = 'none';
+		deploy_btn.setAttribute('data-state', 'rolled');
+		deploy_btn.style.rotate = '360deg';
+	}
+};
 
-    if ( deploy_btn.getAttribute('data-state') == 'rolled' ){
-        options_list.style.display = 'flex';
-        options_list.style.opacity = 1;
-        deploy_btn.setAttribute('data-state', 'deployed');
-        deploy_btn.style.rotate = '180deg';
-    } else{
-        options_list.style.opacity = 0;
-        options_list.style.display = 'none';
-        deploy_btn.setAttribute('data-state', 'rolled');
-        deploy_btn.style.rotate = '360deg';
-    }
+function selectOption() {
+	let current = options_list.querySelector('.current');
+	current.classList.remove('current');
+	this.classList.add('current');
+	deploy_btn.setAttribute('data-state', 'rolled');
+	options_list.style.opacity = 0;
+	options_list.style.display = 'none';
+	deploy_btn.style.rotate = '360deg';
+
+	let country = this.querySelector('span').innerHTML;
+	let flag = this.querySelector('img').src;
+
+	let event = new CustomEvent('optionSelected', { detail: { country: country, flag: flag } });
+	select.dispatchEvent(event);
 }
 
-function selectOption(){
-    let current = options_list.querySelector('.current');
-    current.classList.remove('current');
-    this.classList.add('current');
-    deploy_btn.setAttribute('data-state', 'rolled');
-    options_list.style.opacity = 0;
-    options_list.style.display = 'none';
-    deploy_btn.style.rotate = '360deg';
+select.addEventListener('optionSelected', setNewCurentOpton);
 
-    let country = this.querySelector('span').innerHTML;
-    let flag = this.querySelector('img').src;
-    
-    let event = new CustomEvent("optionSelected", {detail: { country: country, flag: flag } });
-    select.dispatchEvent(event);
-
-}
-
-select.addEventListener("optionSelected", setNewCurentOpton);
-
-function setNewCurentOpton(event){
-    value_wrap.querySelector('img').src = event.detail.flag;
-    value_wrap.querySelector('span').innerHTML = event.detail.country;
-    country.value = event.detail.country;
+function setNewCurentOpton(event) {
+	value_wrap.querySelector('img').src = event.detail.flag;
+	value_wrap.querySelector('span').innerHTML = event.detail.country;
+	country.value = event.detail.country;
 }
 
 //deploy_btn.getAttribute('data-state');
